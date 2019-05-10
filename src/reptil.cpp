1d5fa3fba2060fc8c973977acbfa8df8f9842bcd
Wellyson Gustavo <wellysongustavo@gmail.com>
2019-05-10 08:33:28 -0300

    Separando reptil_nativo.cpp do reptil.cpp

--- src/reptil.cpp
+++ src/reptil.cpp
@@ -1,7 +1,5 @@
 #include "animal.h"
 #include "reptil.h"
-#include "reptil_nativo.h"
-#include "animal_nativo.h"
 
 Reptil::Reptil(int id, std::string classe, std::string nome_cientifico, 
 			char sexo, double tamanho, std::string dieta, Veterinario veterinario, 
@@ -16,14 +14,3 @@ void Reptil::setVenenoso(bool venenoso) { m_venenoso = venenoso; }
 void Reptil::setTipoVenenoso(std::string tipo_venenoso) { m_tipo_venenoso = tipo_venenoso; }
 bool Reptil::getVenenoso() { return m_venenoso; }
 std::string Reptil::getTipoVenenoso() { return m_tipo_venenoso; }
-
-//Reptil nativo
-
-ReptilNativo::ReptilNativo(int id, std::string classe, std::string nome_cientifico, 
-			char sexo, double tamanho, std::string dieta, Veterinario veterinario, 
-			Tratador tratador, std::string nome_batismo, bool venenoso, 
-			std::string tipo_venenoso, std::string autorizacao, std::string uf_origem) : Reptil(id, 
-			classe, nome_cientifico, sexo, tamanho, dieta, veterinario, tratador, nome_batismo, venenoso,  
-			tipo_venenoso), AnimalNativo(autorizacao, uf_origem)  { /* void */ }
-
-ReptilNativo::~ReptilNativo() { /* void */ }
---
+++ src/reptil_nativo.cpp
@@ -0,0 +1,13 @@
+#include "animal.h"
+#include "reptil.h"
+#include "reptil_nativo.h"
+#include "animal_nativo.h"
+
+ReptilNativo::ReptilNativo(int id, std::string classe, std::string nome_cientifico, 
+			char sexo, double tamanho, std::string dieta, Veterinario veterinario, 
+			Tratador tratador, std::string nome_batismo, bool venenoso, 
+			std::string tipo_venenoso, std::string autorizacao, std::string uf_origem) : Reptil(id, 
+			classe, nome_cientifico, sexo, tamanho, dieta, veterinario, tratador, nome_batismo, venenoso,  
+			tipo_venenoso), AnimalNativo(autorizacao, uf_origem)  { /* void */ }
+
+ReptilNativo::~ReptilNativo() { /* void */ }
