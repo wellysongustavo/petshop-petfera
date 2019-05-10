76f67dbafc722814118f981dd23e248766acb074
Wellyson Gustavo <wellysongustavo@gmail.com>
2019-05-09 22:53:56 -0300

    Correção construtor reptil_nativo

--- src/reptil.cpp
+++ src/reptil.cpp
@@ -22,8 +22,8 @@ std::string Reptil::getTipoVenenoso() { return m_tipo_venenoso; }
 ReptilNativo::ReptilNativo(int id, std::string classe, std::string nome_cientifico, 
 			char sexo, double tamanho, std::string dieta, Veterinario veterinario, 
 			Tratador tratador, std::string nome_batismo, bool venenoso, 
-			std::string tipo_venenoso, std::string autorizacao, std::string uf_origem) : Animal(id, classe, nome_cientifico, sexo, 
-			tamanho, dieta, veterinario, tratador, nome_batismo), m_venenoso(venenoso),
-			Reptil(tipo_venenoso), AnimalNativo(autorizacao, uf_origem)  { /* void */ }
+			std::string tipo_venenoso, std::string autorizacao, std::string uf_origem) : Reptil(id, 
+			classe, nome_cientifico, sexo, tamanho, dieta, veterinario, tratador, nome_batismo, venenoso,  
+			tipo_venenoso), AnimalNativo(autorizacao, uf_origem)  { /* void */ }
 
 ReptilNativo::~ReptilNativo() { /* void */ }
