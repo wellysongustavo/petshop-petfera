645bf607437ec57e65450c577caa24675b221a4b
Pedro Hugo <47755506+PedroHugo11@users.noreply.github.com>
2019-05-16 15:21:17 -0300

    Adicionande mamiferos e aves exoticas e nativas .h

---
+++ mamifero_nativo.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "mamifero.h"
+#include "animal_nativo.h"
+#include "animal_silvestre.h"
+
+
+#ifndef MAMIFERO_NATIVO_H
+#define MAMIFERO_NATIVO_H
+
+class MamiferoNativo: public Mamifero, AnimalNativo{
+	public:
+		MamiferoNativo(int id, std::string classe, std::string nome_cientifico, 
+			char sexo, double tamanho, std::string dieta, Veterinario veterinario, 
+            Tratador tratador, std::string nome_batismo, std::string cor_pelo, 
+            std::string autorizacao, std::string uf_origem);
+		~MamiferoNativo();
+};
+
+#endif
